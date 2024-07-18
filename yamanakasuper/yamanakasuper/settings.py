c7fe1d05cda7cb01d1ddc7b767eeec888423bac1
yamanaka8338yuki <y_yuki0803@outlook.jp>
2024-07-19 01:09:40 +0900

    サインアップ機能を追加

--- yamanakasuper/yamanakasuper/settings.py
+++ yamanakasuper/yamanakasuper/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "app.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
