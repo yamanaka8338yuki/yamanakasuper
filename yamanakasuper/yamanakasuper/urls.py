1f34509f7104b91aa570eebc562335057aaa13cf
yamanaka8338yuki <y_yuki0803@outlook.jp>
2024-07-18 23:37:16 +0900

    各ページにリンクを追加

--- yamanakasuper/yamanakasuper/urls.py
+++ yamanakasuper/yamanakasuper/urls.py
@@ -20,10 +20,10 @@ from app.views import TopView, SignupView, LoginView, HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TopView.as_view()),
-    path('signup/', SignupView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('home/', HomeView.as_view()),
+    path('', TopView.as_view(), name="top"),
+    path('signup/', SignupView.as_view(), name="signup"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('home/', HomeView.as_view(), name="home"),
     
 
 ]
