ba68d6f40c174b06c668d33e6b5fdbc863979297
yamanaka8338yuki <y_yuki0803@outlook.jp>
2024-08-14 23:40:33 +0900

    ログイン処理の実装

--- yamanakasuper/yamanakasuper/settings.py
+++ yamanakasuper/yamanakasuper/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = "app.User"
+AUTHENTICATION_BACKENDS = [
+    "app.backends.UserAuthBackend"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
